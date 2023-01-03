f17d005c86b085b633ab629d35eeb13c012c1449
Andi Mohamad Sultan <sultan@Andis-MacBook-Pro.local>
2023-01-03 09:53:41 +0700

    init demo page

--- src/components/dropdown/dropdown.theme.interface.tsx
+++
@@ -1,9 +0,0 @@
-export interface dropdownThemeInterface {
-  padding: string
-  background: string
-  transition: string
-  boxShadow: string
-  border: string
-  borderRadius: string
-  top: string
-}
